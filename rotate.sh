ca72fb7a18de9f76c032b182655d293aa15e4d94
Dan Lazewatsky <dan@lazewatsky.com>
2014-10-01 17:33:47 -0500

    fix variable declarations, and only delete stuff if rsync completed successfully

--- rotate.sh
+++ rotate.sh
@@ -1,11 +1,18 @@
 #!/usr/bin/env sh
 
-BAG_DIR = $1
-REMOTE_HOST = $2
-REMOTE_DIR = $3
+BAG_DIR=$1
+REMOTE_HOST=$2
+REMOTE_DIR=$3
 
 # rsync over all the files (let it try old files too)
 rsync $BAG_DIR/*.bag $REMOTE_HOST:$REMOTE_DIR
+status=$?
 
-# remove files older than one week
-find $BAG_DIR/* -mtime 7 -exec rm {} \;
+if test $status -eq 0
+then
+	# remove files older than one week
+	find $BAG_DIR/* -mtime 7 -exec rm {} \;
+else
+	# something went wrong - send an email maybe
+	exit 1;
+fi
